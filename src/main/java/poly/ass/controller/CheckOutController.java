ff76d862eaffbc9d0aa5d36af4ee487ea433132c
namntph11607 <67990613+namntph11607@users.noreply.github.com>
2021-06-18 23:56:35 +0700

    Update CheckOutController.java

--- src/main/java/poly/ass/controller/CheckOutController.java
+++ src/main/java/poly/ass/controller/CheckOutController.java
@@ -1,15 +1,21 @@
 package poly.ass.controller;
 
+import java.io.File;
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +28,7 @@ import poly.ass.dao.ProductDAO;
 import poly.ass.entity.Account;
 import poly.ass.entity.CartItem;
 import poly.ass.entity.Category;
+import poly.ass.entity.MailModel;
 import poly.ass.entity.Order;
 import poly.ass.entity.OrderDetail;
 import poly.ass.entity.Product;
@@ -48,7 +55,10 @@ public class CheckOutController {
 	@Autowired
 	SessionService session;
 	
-	@RequestMapping("/cart/checkout")
+	@Autowired
+	JavaMailSender sender;
+	
+	@GetMapping("/cart/checkout")
 	public String checkout(
 			Model model
 			) {
@@ -72,7 +82,6 @@ public class CheckOutController {
 	public String postCheckOut(
 			Model model,
 			@Validated @ModelAttribute("order") Order order, 
-			@RequestParam("fullname") String fullname,
 			BindingResult result
 			) {
 		model.addAttribute("TitlePage", "CheckOut Details");
@@ -88,16 +97,13 @@ public class CheckOutController {
 			return "redirect:/cart/checkout";
 		}else {
 				//tao order moi
-//				daoOrder.save(new Order());
-				System.out.println("Save order: " + order);
-				System.out.println("New order fullname: " + fullname);
-				
+				daoOrder.save(order);
 				
 				//luu du lieu order detail
 
 				for (CartItem item : cart.getItems()) {
-					System.out.println("Save orderDtails: " + item);
-					Order o = daoOrder.getById(7);
+//					System.out.println("Save orderDtails: " + item);
+					Order o = daoOrder.getById(order.getId());
 					Product product = daoProduct.findByName(item.getProductName());
 
 					daoOrderDetail.save(new OrderDetail(item.getPrice(), item.getQuantity(), product, o));
@@ -111,4 +117,26 @@ public class CheckOutController {
 		
 //		return "redirect:/cart/checkout";
 	}
+	
+//	public void sendEmail() throws MessagingException {
+//		MimeMessage message = sender.createMimeMessage();
+//		MailModel mail = new MailModel();
+//		//viet thu
+//		sender.send(message); // bo vao buu dien
+//		MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+//		helper.setFrom(mail.getFrom());
+//		helper.setTo(mail.getTo());
+//		helper.setSubject(mail.getSubject());
+//		helper.setText(mail.getBody(), true);
+//		helper.setReplyTo(mail.getFrom());
+//		for(String email : mail.getCc()) {
+//			helper.addCc(email);
+//		}
+//		for(String email : mail.getBcc()) {
+//			helper.addBcc(email);
+//		}
+//		for(File file : mail.getFiles()) {
+//			helper.addAttachment(file.getName(), file);
+//		}
+//	}
 }
