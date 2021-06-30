96e01669053be2ac33be273dd15abac1efb2cbad
namntph11607 <67990613+namntph11607@users.noreply.github.com>
2021-06-30 12:06:51 +0700

    Delete src directory

--- src/main/java/poly/ass/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package poly.ass.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import poly.ass.entity.Category;
-
-public interface CategoryDAO extends JpaRepository<Category, Integer> {
-
-}
--- src/main/java/poly/ass/dao/OrderDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.ass.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.ass.entity.Order;
-
-public interface OrderDAO extends JpaRepository<Order, Integer> {
-
-}
